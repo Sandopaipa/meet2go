545ac9288ecd1e8681fdb24f839f0057e53e583e
Sandopaipa <kirill.kuli.01@gmail.com>
2022-10-08 23:09:36 +0300

    Initial commit for events service

--- dockerfile
+++ dockerfile
@@ -1,8 +1,10 @@
 # syntax=docker/dockerfile:1
-FROM python:lastest
+FROM python:latest
 ENV PYTHONDONTWRITEBYTECODE=1
 ENV PYTHONUNBUFFERED=1
 WORKDIR /meet2go
-COPY requiremenets.txt /meet2go/
-RUN pip install -r requiremenets.txt
-COPY . /meet2go/
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
+COPY . /meet2go
+
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- meet2go/settings.py
+++ meet2go/settings.py
@@ -88,7 +88,17 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
+"""DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'meet2go_db',
+        'USER': 'admin',
+        'PASSWORD': '1111',
+        'HOST': 'db',
+        'PORT': '5432',
+    }
+}
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
